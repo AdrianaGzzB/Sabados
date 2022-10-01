946a89851813e82450646451578347c88a6c624c
Adriana <adrianagb2105@gmail.com>
2022-10-01 14:01:11 -0500

    Add-imagen
    agregamos imagenes y descripcion al stock

--- Ecommerce/stock.js
+++ Ecommerce/stock.js
@@ -10,11 +10,11 @@ let stockProductos = [
     {id: 9, nombre: "Pulsera Dama(xv o boda)", tipo: "pulsera", cantidad: 1, desc: "Pulsera para Damas de honor, color a elegir", precio: 1000, img: './img/pulseraDama(xv-boda).jpg'},
     {id: 10, nombre: "Pies Descalzos", tipo: "banda", cantidad: 1, desc: "Banda de bebe y pies descalzos en colores disponibles", precio: 1000, img: './img/piesDescalzos.jpg'},
     {id: 11, nombre: "Cinturon Baby1", tipo: "cinturon", cantidad: 1, desc: "Cinturon de maternidad con distintivo para el pápa, color a elegir", precio: 1000, talla: "L", img: './img/cinturonBaby1.jpg'},
-    {id: 12, nombre: "Moño 2", tipo: "moño", cantidad: 1, desc: "Broche con flores de diferente color a elegir", precio: 1000, img: './img/moño2.jpg'}
-    {id: 13, nombre: "Cinturon", tipo: "moño", cantidad: 1, desc: "Broche con flores de diferente color a elegir", precio: 1000, img: './img/cinturonBaby4.jpg'}
-    {id: 14, nombre: "Cinturon", tipo: "moño", cantidad: 1, desc: "Broche con flores de diferente color a elegir", precio: 1000, img: './img/cinturonBaby3.jpg'}
-    {id: 15, nombre: "Moño", tipo: "moño", cantidad: 1, desc: "Broche con flores de diferente color a elegir", precio: 1000, img: './img/moño5.jpg'}
-    {id: 16, nombre: "Banda", tipo: "Banda", cantidad: 1, desc: "Broche con flores de diferente color a elegir", precio: 1000, img: './img/bautizo1.jpg'}
+    {id: 12, nombre: "Moño 2", tipo: "moño", cantidad: 1, desc: "Broche con flores de diferente color a elegir", precio: 1000, img: './img/moño2.jpg'},
+    {id: 13, nombre: "Cinturon", tipo: "cinturon", cantidad: 1, desc: "Cinturon de maternidad para BabyShower", precio: 1000, img: './img/cinturonBaby4.jpg'},
+    {id: 14, nombre: "Cinturon", tipo: "cinturon", cantidad: 1, desc: "Cinturon de maternidad para BabyShower", precio: 1000, img: './img/cinturonBaby3.jpg'},
+    {id: 15, nombre: "Moño", tipo: "moño", cantidad: 1, desc: "Moño personalizado para niña", precio: 1000, img: './img/moño5.jpg'},
+    {id: 16, nombre: "Banda", tipo: "Banda", cantidad: 1, desc: "Banda y pies descalzos para Bautizo", precio: 1000, img: './img/bandabautizo1.jpg'}
     
     
 ]
