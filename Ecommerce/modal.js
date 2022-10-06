d9f332edd74812b9d2d3497bb6852973232376b1
Adriana <adrianagb2105@gmail.com>
2022-10-06 17:48:55 -0500

    cambio titulo
    se cambio el titulo dentro del HTML

--- Ecommerce/modal.js
+++ Ecommerce/modal.js
@@ -19,7 +19,7 @@ const modalCarrito = document.getElementsByClassName('modal-carrito')[0];
 const botonAbrir = document.getElementById('boton-carrito');
 const botonCerrar = document.getElementById('carritoCerrar')
 botonAbrir.addEventListener('click', ()=>{
-    contenedorModal.classList.toggle('modal-active')
+    contenedorModal.classList.toggle('modal-active');
 });
 botonCerrar.addEventListener('click', ()=>{
     contenedorModal.classList.toggle('modal-active')
